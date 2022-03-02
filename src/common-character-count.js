a488380707e3325af1c3583d20a8586ee5a80369
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-03-02 14:22:08 +0500

    add task

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  const arrS1 = s1.split('');
+  const arrS2 = s2.split('');
+  for (let i = 0; i < arrS1.length; i++) {
+    for (let j = 0; j < arrS2.length; j++) {
+      if (arrS1[i] == arrS2[j]) {
+        count += 1;
+        arrS1.splice(arrS1[i], 1);
+        arrS2.splice(arrS2[j], 1);
+      }
+    }
+  }
+  return count
 }
 
 module.exports = {
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disksNumber, turnsSpeed) {
+  const result = {};
+  let a = (2 ** disksNumber) - 1;
+  let b = Math.floor(a / (turnsSpeed/3600))
+  result.turns = a;
+  result.seconds = b;
+
+  return result;
 }
 
 module.exports = {
