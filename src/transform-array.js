2811e623e24674701d7dab9a77aede1e36a9fd7a
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-03-02 12:54:55 +0500

    feat: add new task

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  if (!Array.isArray(arr)) {
+  throw new Error('\'arr\' parameter must be an instance of the Array!')
+  }
+
+  const result = [];
+  const cloneArr = [...arr];
+
+  for (let i = 0; i < cloneArr.length; i++) {
+    if (typeof(cloneArr[i]) == 'number') {
+      result.push(cloneArr[i]);
+    }
+    if ((cloneArr[i] == '--discard-next') && (i < i.length - 1)) {
+      i += 1;
+    }
+    if ((cloneArr[i] == '--discard-prev') && (i > 0)) {
+      result.pop();
+    }
+    if ((cloneArr[i] == '--double-next')  && (i < i.length - 1)) {
+      console.log(i)
+      result.push(cloneArr[i+1]);
+    }
+    if ((cloneArr[i] == '--double-prev') && (i > 0)) {
+      result.push(cloneArr[i-1]);
+    }
+    if ((cloneArr[i] == '--double-prev') && (cloneArr[i-2] == '--discard-next')) {
+      result.pop();
+      result.pop();
+    }
+    if ((cloneArr[i] == '--double-prev') && (cloneArr[i-2] == '--double-next')) {
+      result.push(cloneArr[i-1]);
+    }
+    if ((cloneArr[i] == '--discard-prev') && (cloneArr[i-2] == '--double-next')) {
+      result.push(cloneArr[i-1]);
+    }
+  }
+  return result;
 }
 
 module.exports = {
