49ec7b44f5226a3b7b6789521f97bebaa76c2a98
Alexey Ponomarev <ponomarevmgn@gmail.com>
2022-03-05 14:21:32 +0500

    add new task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,56 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+  let sep = '';
+
+  if (options.addition === null) {
+    options.addition = "null"
+  }
+
+  if ((options.addition !== undefined) & (options.additionRepeatTimes == undefined) & (options.additionSeparator == undefined)) {
+    sep += options.addition;
+  }
+
+  if ((options.addition !== undefined) & (options.additionRepeatTimes !== undefined) & (options.additionSeparator == undefined)) {
+    const arr1 = [];
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      arr1.push(options.addition);
+    }
+    sep = arr1.join('|');
+  }
+
+  if ((options.addition !== undefined) & (options.additionRepeatTimes !== undefined) & (options.additionSeparator !== undefined)) {
+    const arr2 = [];
+    for (let j = 0; j < options.additionRepeatTimes; j++) {
+      arr2.push(options.addition);
+      }
+    sep = arr2.join(options.additionSeparator);
+  }
+
+  if ((options.addition !== undefined) & (options.additionRepeatTimes == undefined) & (options.additionSeparator !== undefined)) {
+    sep = options.addition;
+  }
+
+  str += sep;
+
+  if ((options.repeatTimes !== undefined) & (options.separator == undefined)) {
+    const arr3 = [];
+    for (let k = 0; k < options.repeatTimes; k++) {
+      arr3.push(str);
+    }
+    str = arr3.join('+');
+  }
+
+  if ((options.repeatTimes !== undefined) & (options.separator !== undefined)) {
+    const arr4 = [];
+    for (let m = 0; m < options.repeatTimes; m++) {
+      arr4.push(str);
+    }
+    str = arr4.join(options.separator);
+  }
+
+  return str;
 }
 
 module.exports = {
